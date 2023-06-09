7abf2473b16c00707618212752517ceb109e52b5
kishor-kumar-parida <126803569+kishor-kumar-parida@users.noreply.github.com>
2023-06-09 19:41:03 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,46 @@
+const inputBox = document.getElementById('input-box');
+const listContainer = document.getElementById('list-container');
+
+function addTask() {
+    if (inputBox.value === "") {
+        alert("Please enter a task");
+    }
+    else {
+        let li = document.createElement("li");
+        li.innerHTML = inputBox.value;
+        listContainer.appendChild(li);
+        let span = document.createElement("span");
+        span.innerHTML = "\u00d7";
+        li.appendChild(span);
+    }
+    inputBox.value = "";
+    savedata();
+}
+
+listContainer.addEventListener("click", function (e) {
+    if (e.target.tagName === "LI") {
+        e.target.classList.toggle("checked");
+        savedata()
+    }
+    else if (e.target.tagName === "SPAN") {
+        e.target.parentElement.remove();
+        savedata()
+    }
+}, false);
+
+function savedata() { 
+    localStorage.setItem("data", listContainer.innerHTML);
+}
+
+function showTask(){
+    listContainer.innerHTML = localStorage.getItem("data");
+}
+
+showTask();
+
+inputBox.addEventListener("keyup", function (event) {
+    if (event.keyCode === 13) { // 13 is the keycode for "Enter"
+        event.preventDefault();
+        addTask();
+    }
+})
